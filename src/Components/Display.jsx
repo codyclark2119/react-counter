f564b53c98462ea0d2d04d98f32cc3856ece378d
cody clark <codyclark2119@gmail.com>
2020-06-02 11:56:23 -0500

    Finished by adding css animation library

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FlipNumbers from 'react-flip-numbers';
 import { useCountContext } from "../utils/GlobalState";
 
 function Display() {
@@ -6,12 +7,12 @@ function Display() {
 
     return (
         <section className="display">
-            <div id='counter'>{state.count}</div>
-            Your count history:
+            <div id='counter'><FlipNumbers height={60} width={60} numberStyle={{fontSize: '10px'}} color="white" background="black" play perspective={200} numbers={state.count.toString()} /></div>
+            <u>Your count history:</u>
             <aside id='history'>
                 <div >
-                    {state.hist.map(num => {
-                      return <p>{`${num}`}</p>  
+                    {state.hist.map((num, i) => {
+                      return <p>{`${i + 1}. ${num}`}</p>  
                     })}
                 </div>
             </aside>
